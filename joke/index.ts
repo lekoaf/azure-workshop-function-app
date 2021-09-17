9e8a08b01c3a9e365e5244c73704772db9e4a847
Martin Trobäck <martintk@axis.com>
2021-09-17 11:26:52 +0200

    feat: array of jokes

--- joke/index.ts
+++ joke/index.ts
@@ -8,7 +8,22 @@ interface Response {
   };
 }
 
-const jokeOfTheDay = "I invented a new word! Plagiarism!";
+const jokeOfTheDay = [
+  "I invented a new word! Plagiarism!",
+  "Did  you hear about the calustrophobic astronaut? He just needed a little space.",
+  "How do you drown a hipster? Throw him in the mainstream.",
+  "What kind of exercise do lazy people do? Diddly-squats.",
+  "What do you call a pony with a cough? A little horse!",
+  "What is Forrest Gump's password? 1Forrest1.",
+  "Why did the M&M go to school? He wanted to be a Smartie.",
+  "What did one traffic light say to the other? Stop looking at me, I'm changing!",
+  "What do you call bears with no ears? B.",
+  "What's a foot long and slippery? A slipper!",
+  "Why do French people eat snails? They don't like fast food!",
+  "What's red and moves up and down? A tomato in an elevator!",
+  "I invented a new word today: Plagiarism.",
+  "What is sticky and brown? A stick!",
+];
 
 const httpTrigger: AzureFunction = async function (
   context: Context,
@@ -16,9 +31,11 @@ const httpTrigger: AzureFunction = async function (
 ): Promise<void> {
   context.log.info("Hello from joke function!");
 
+  const joke = jokeOfTheDay[Math.floor(Math.random() * jokeOfTheDay.length)];
+
   context.res = {
     status: 200,
-    body: { text: jokeOfTheDay },
+    body: { text: joke },
     headers: {
       "Content-Type": "application/json",
     },
